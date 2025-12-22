a209368c1ad555c29e9f0fefa18243d429b18b78
Tuan Dinh <tuan98dinh@gmail.com>
2025-12-22 15:26:52 -0500

    new commit

---
+++ Tuan/RPS.js
@@ -0,0 +1,12 @@
+
+<html lang="EN">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Rock Paper Sciszors</title>
+        <body>
+            <script>
+                let choice = prompt("Enter Choice");
+            </script>
+        </body>
+    </head>
+</html>
