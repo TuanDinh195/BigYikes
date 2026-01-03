fc25304fc90198cc8f44b1bfaacbcb36af2eb144
gisellerahimi-dotcom <gisellerahimi@gmail.com>
2026-01-03 13:15:35 -0500

    learning how to create buttons

---
+++ gptPractice/script.js
@@ -0,0 +1,13 @@
+
+
+    // function sayHello() {
+    //     alert("Hello! The button was clicked.");
+    // }
+
+
+    //my_array = ["apples", "oranges", "pears"]
+
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        alert("Hello, you are doing well")
+    });
